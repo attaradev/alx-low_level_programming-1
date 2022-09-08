866bb62b2aeacfa481fb9a5ccd3fa5dcb137d875
Ashdof <mzigah4@gmail.com>
2022-09-08 10:32:51 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-	writef(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dore Korpar, 2015-10-19\n");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 	return (1);
 }
