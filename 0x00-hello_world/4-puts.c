18fe814bcfcb3a77ecb48d2553ec26dd0c5c9e24
Ashdof <mzigah4@gmail.com>
2022-09-08 08:05:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 /*
  * main - Entry point
  *
- * Description: 'C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.'
+ * Description: 'C program that prints exactly Programming is like building 
+ * a multilingual puzzle, followed by a new line.'
  *
  * Return: Always 0 (Success)
  *
