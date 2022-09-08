1b3da8d5a1ac9e5e9e612cfc95fef492f67fcdc8
Ashdof <mzigah4@gmail.com>
2022-09-08 09:00:19 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Description: This programme prints exactly Programming is like
  * building a multilingual puzzle, followed by a new line.
  *
- * return: always 0 (Success)
+ * Return: return 0
  *
  */
 int main(void)
