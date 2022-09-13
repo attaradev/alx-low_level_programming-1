95689b131781532d1550e759854d13668e4b1c7f
Ashdof <mzigah4@gmail.com>
2022-09-13 10:09:39 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,10 @@
  * @c: Character to check
  *
  * Description: Checks for alphabetic characters
+ *
+ * Return: Returns 0 or 1
  */
 int _isalpha(int c)
 {
-	return ((c >= 97 && c <= 122) || (c>= 65 && c <= 90));
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
