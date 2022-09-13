271947f682b0def8560c0b3eb98d5b3528dfef6b
Ashdof <mzigah4@gmail.com>
2022-09-13 11:22:01 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,13 @@
 int print_last_digit(int n)
 {
 	int lastDigit;
-    
-	lastDigit = n % 10;
 
+	lastDigit = n % 10;
+	if (lastDigit < 0)
+	{
+		lastDigit *= (-1);
+	}
+
+	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
