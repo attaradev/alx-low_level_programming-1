c146efd548e7c3f8aacfacf04fabe39b92339ce6
Ashdof <mzigah4@gmail.com>
2022-09-14 02:13:01 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Test function that prints if integer is positive or negative
+ * Return: Return 0
+ */
+int main(void)
+{
+	int i;
+	i = 98;
+
+	if (i == "" || i == "")
+	{
+		printf("Enter a number!");
+	}
+	else
+	{
+		positive_or_negative(i);
+	}
+
+	return (0);
+}
