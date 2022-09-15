9ca09e2dbfc2cca5d9a123dd92b0cbd547ffbdd8
Ashdof <mzigah4@gmail.com>
2022-09-15 08:40:48 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,14 @@
 
 void print_numbers(void)
 {
-	int i = 0;
-
-	while (i <= 9)
-	{
-		_putchar(i);
-		i++;
-	}
+	_putchar('0');
+	_putchar('1');
+	_putchar('2');
+	_putchar('3');
+	_putchar('4');
+	_putchar('5');
+	_putchar('6');
+	_putchar('7');
+	_putchar('8');
+	_putchar('9');
 }
