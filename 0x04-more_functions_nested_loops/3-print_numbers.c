2b7882a4192c32f05c51484589306cff4fbfed06
Ashdof <mzigah4@gmail.com>
2022-09-15 08:24:52 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers to the std
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		i++;
+	}
+}
