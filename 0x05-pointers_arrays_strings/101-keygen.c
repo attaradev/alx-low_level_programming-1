3be1ff03d4b18f1897f782b3858f4378f9322b49
Ashdof <mzigah4@gmail.com>
2022-09-19 11:48:31 -0700

    Task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,11 +11,26 @@
 
 int main(void)
 {
-	int pass;
+	char password[100];
+	int randNum, num, i;
+
+	num = 0;
+	i = 0;
 
 	srand(time(NULL));
 
-	pass = rand() % 100;
+	while (num < 2645)
+	{
+		randNum = random() % 127;
+		if (randNum > 32)
+		{
+			password[i++] = randNum;
+			num += randNum;
+		}
+	}
+	password[i++] = (2772 - num);
+	password[i] = '\0';
+	printf("%s\n", password);
 
-	return (pass);
+	return (0);
 }
