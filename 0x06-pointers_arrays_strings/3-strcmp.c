59c67258401228b911ff20f752163027fe7abc2c
Ashdof <mzigah4@gmail.com>
2022-09-21 06:42:19 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	
-	for (; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 == '\0')
 		{
-			return s1[i] - s2[i];
+			return (0);
 		}
+		s1++;
+		s2++;
 	}
 
-	return (0);
+	return (*s1 - *s2);
 }
